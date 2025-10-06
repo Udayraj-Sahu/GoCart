6414966b04bfe7cc8d10c79b57bc67f9b1e0cb1f
Udayraj Sahu <96167329+Udayraj-Sahu@users.noreply.github.com>
2025-10-07 02:35:04 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,11 +2,12 @@ import {
 	syncUserCreation,
 	syncUserDeletion,
 	syncUserUpdation,
-} from "@/inngest/client";
+} from "@/inngest/functions";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
 	client: inngest,
 	functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
 });
+
 
